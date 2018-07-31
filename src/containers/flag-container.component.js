b03fc64f4576a74a0265a9d6c43750afd1e53e92
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-07-31 16:16:18 +0200

    Update flag-container.component.js

--- src/containers/flag-container.component.js
+++ src/containers/flag-container.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CountryFlagList from '../presentational/flag-list.component';
-import { getCountries } from '../actions/actions-countries';
+import { getCountries, searchCountries } from '../actions/actions-countries';
 import '../country.css';
 
 class CountryFlagContainer extends Component {
@@ -13,10 +13,17 @@ class CountryFlagContainer extends Component {
         this.props.dispatch(getCountries());
     }
 
+    search(event) {
+        this.props.dispatch(searchCountries(event.target.value));
+    }
+
     render() {
         return (
             <div>
-                <CountryFlagList countries={this.props.countries} />
+                <div className="search text-center">
+                    <input type="text" onChange={this.search.bind(this)}/>
+                </div>
+                <CountryFlagList countries={this.props.visibleCountries} />
             </div>
         )
     }
@@ -24,7 +31,8 @@ class CountryFlagContainer extends Component {
 
 const mapStateToProps = function (store) {
     return {
-        countries: store.countriesReducer.countries
+        countries: store.countriesReducer.countries,
+        visibleCountries: store.countriesReducer.visibleCountries
     };
 };
 
