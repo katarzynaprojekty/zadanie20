150f6f307892800234a8e85b7c23e3e0b8d9ee1d
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-07-31 16:01:30 +0200

    Update countries-reducer.js

--- src/reducers/countries-reducer.js
+++ src/reducers/countries-reducer.js
@@ -3,14 +3,15 @@ import countriesData from '../data/countries.json';
 
 const initialState = {
     countries: countriesData,
-    selectedCountry: {}
+    selectedCountry: {},
+    visibleCountries: []
 };
 
 const countriesReducer = function (state = initialState, action) {
     switch (action.type) {
         case GET_COUNTRIES:
             return Object.assign({}, state, {countries: state.countries});
-            
+
         case GET_COUNTRY:
             const selectedCountry= state.countries.find(country => country.id===parseInt(action.id));
             return Object.assign({}, state, {selectedCountry});
